0d8a0c95c024819055feac4aaca81a968a80bfbd
arjunbky <arjunbky@gmail.com>
2022-12-30 10:13:33 +0530

    structure

---
+++ structure.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+struct square{
+int x;
+int y;
+}s1;
+int main(){
+
+    s1={1,1};
+
+    cout<<s1.x<<endl;
+    cout<<s1.y<<endl;
+
+}
